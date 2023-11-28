02c7c260275eba13ff3e3d0d00c48ad0c80f6e3c
sinaloa10 <oscarsinaloa47@gmail.com>
2023-11-27 21:43:16 -0600

    Servidor jalando

--- conexion.js
+++ conexion.js
@@ -10,12 +10,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const config = {
-    server: "DESKTOP-ENVLS9T",
+    server: "DESKTOP-36GHE4O",
     authentication: {
         type: "default",
         options: {
-            userName: "Oscar",
-            password: "NuevaContraseña"
+            userName: "Carlos",
+            password: "ViejaContraseña"
         }
     },
     options: {
@@ -48,7 +48,6 @@ app.get('/registro/registrar.html', (req, res) => {
 app.use(express.static(__dirname));
 
 
-// Ruta para la inserción de registros
 app.post('/registro/registrar', (req, res) => {
     const { name, lastname, email, password } = req.body;
 
@@ -62,13 +61,101 @@ app.post('/registro/registrar', (req, res) => {
         } else {
             res.status(200).send('Registro exitoso');
         }
-        connection.close();
+        // No cierres la conexión aquí
     });
+    
 
-    request.addParameter('Nombre', TYPES.VarChar, name);
+    request.addParameter('Nombre', TYPES.VarChar, name) ;
     request.addParameter('Apellido', TYPES.VarChar, lastname);
     request.addParameter('Correo', TYPES.VarChar, email);
     request.addParameter('Contraseña', TYPES.VarChar, password);
 
+    // Mueve esta línea fuera del evento 'requestCompleted'
     connection.execSql(request);
+
+    // Cierra la conexión después de completar la solicitud
+    connection.on('requestCompleted', function () {
+        connection.close();
+    });
+});
+
+
+// Ruta para el formulario de registro
+app.get('/registro/registrar.html', (req, res) => {
+    res.sendFile(__dirname + '/registro/registrar.html');
+});
+
+app.use(express.static(__dirname));
+
+
+app.post('/registro/registrar', (req, res) => {
+    const { name, lastname, email, password } = req.body;
+
+    const request = new Request(`
+        INSERT INTO Usuarios (Nombre, Apellido, Correo, Contraseña)
+        VALUES (@Nombre, @Apellido, @Correo, @Contraseña);
+    `, (err, rowCount) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error al insertar el registro en la base de datos');
+        } else {
+            res.status(200).send('Registro exitoso');
+        }
+        // No cierres la conexión aquí
+    });
+    
+
+    request.addParameter('Nombre', TYPES.VarChar, name) ;
+    request.addParameter('Apellido', TYPES.VarChar, lastname);
+    request.addParameter('Correo', TYPES.VarChar, email);
+    request.addParameter('Contraseña', TYPES.VarChar, password);
+
+    // Mueve esta línea fuera del evento 'requestCompleted'
+    connection.execSql(request);
+
+    // Cierra la conexión después de completar la solicitud
+    connection.on('requestCompleted', function () {
+        connection.close();
+    });
+});
+
+
+
+// Ruta para el formulario de inicio de sesión
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta para el inicio de sesión
+app.post('/iniciar-sesion', (req, res) => {
+    const { email, password } = req.body;
+
+    const request = new Request(`
+        SELECT Correo, Contraseña FROM Usuarios WHERE Correo = @Correo AND Contraseña = @Contraseña;
+    `, (err, rowCount) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error al realizar el inicio de sesión');
+        } else {
+            if (rowCount > 0) {
+                // Inicio de sesión exitoso
+                res.redirect('/menu/menu.html'); // Redirige a la página de menú
+            } else {
+                // Credenciales incorrectas
+                res.status(401).send('Correo electrónico o contraseña incorrectos');
+            }
+        }
+        // No cierres la conexión aquí
+    });
+
+    request.addParameter('Correo', TYPES.VarChar, email);
+    request.addParameter('Contraseña', TYPES.VarChar, password);
+
+    // Mueve esta línea fuera del evento 'requestCompleted'
+    connection.execSql(request);
+
+    // Cierra la conexión después de completar la solicitud
+    connection.on('requestCompleted', function () {
+        connection.close();
+    });
 });
